0abd602c6ae5d1d898dbf8628d83a6bf3e73dc25
trinhlenhathuy <72062264+trinhlenhathuy@users.noreply.github.com>
2024-08-09 13:57:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,23 +6,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='analytics_huytln',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'pandas',
         'matplotlib',
     ],
-    description='A simple library to plot Insightful charts',
+    description='A simple library to plot insightful charts',
     long_description=long_description,  # Thêm mô tả dài
     long_description_content_type="text/markdown",  # Định dạng của README.md
     author='Huy Trịnh Lê Nhật',
     author_email='trinhlenhathuy@gmail.com',
-    # url='https://github.com/trinhlenhathuy/huytln_visualization',
+    url='https://github.com/trinhlenhathuy/huytln_visualization',  # URL dự án
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    license='MIT',  # Giấy phép của dự án
+    keywords='data visualization, charts, pareto, heatmap',  # Từ khóa tìm kiếm
 )
-
