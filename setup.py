134b620cee5406865387fe32f982e98a0ee0b933
trinhlenhathuy <72062264+trinhlenhathuy@users.noreply.github.com>
2024-08-09 11:36:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='analytics_huytln',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'pandas',
         'matplotlib',
     ],
-    description='A simple library to plot Pareto charts',
+    description='A simple library to plot Insightful charts',
     author='Huy Trịnh Lê Nhật',
     author_email='trinhlenhathuy@gmail.com',
     # url='https://github.com/trinhlenhathuy/huytln_visualization',
