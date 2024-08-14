90e57bd48be04c14b56560dadf917b423834e497
trinhlenhathuy <72062264+trinhlenhathuy@users.noreply.github.com>
2024-08-14 16:38:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name='analytics_huytln',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(),
     install_requires=[
         'pandas',
